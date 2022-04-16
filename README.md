00e41248e496e6dbeabe12e5b80f82cb3acf0e7e
Quoc Le <songmai108@gmail.com>
2022-04-17 00:54:48 +0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Getting Started with Simple ERC20 Token Truffle Tutorial
+
+### install
+```
+npm install -g truffle
+
+npm install
+```
+
+
+### run
+```
+# unit-test
+npm run test
+
+$ test coverage
+npm run test:coverage
+
+$ hint
+npm run linter
+```
+
+# Result
---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  contracts_build_directory: './build',
+  networks: {
+    ethereum: {
+      network_id: 31337,
+      host: '127.0.0.1',
+      port: 9545,
+      gasPrice: 0,
+    },
+  },
+  plugins: ["solidity-coverage"],
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+}
